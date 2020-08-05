0845efba9a445fd098e20bc587af71ff29bb829d
amindazad <amindazad@icloud.com>
2020-08-05 15:23:58 -0700

    fixed no_dops

--- applications/no_dups/no_dups.py
+++ applications/no_dups/no_dups.py
@@ -1,7 +1,21 @@
+# def no_dups(str):
+#     return "".join(dict.fromkeys(str))
+
 def no_dups(str):
-    return "".join(dict.fromkeys(str))
+    counts = {}
+    words_list = str.split()
+    new_str = ''
 
+    for word in words_list:
+        if word not in counts:
+            counts[word] = 1
+        else:
+            counts[word] += 1
 
+        if counts[word] == 1:
+            new_str += f'{word} '
+
+    return new_str.rstrip()
 
 if __name__ == "__main__":
     print(no_dups(""))
