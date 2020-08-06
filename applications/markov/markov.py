8203e1b3c54d437fecccb52b74850301b53b7ba7
amindazad <amindazad@icloud.com>
2020-08-06 16:11:33 -0700

    markov done

--- applications/markov/markov.py
+++ applications/markov/markov.py
@@ -5,9 +5,37 @@ with open("input.txt") as f:
     words = f.read()
 
 # TODO: analyze which words can follow other words
-# Your code here
+# Split the words into a list 
+words_list = words.split()
 
+#Iterate through the words and build a dataset of words following eachother
+dataset = {}
+
+for i in range(len(words_list)):
+    if words_list[i] not in dataset:
+        dataset[words_list[i]] = []
+
+    try:
+        dataset[words_list[i]].append(words_list[i+1])
+    except IndexError:
+        break 
 
 # TODO: construct 5 random sentences
-# Your code here
+
+suffix = ('.', '?', '!', '."', '?"', '!"')
+
+def print_random(word):
+    print(word, end= " ")
+
+    if word.endswith(suffix):
+        print('/n')
+        return
+    else:
+        print_random(random.choice(dataset[word]))
+
+print_random('Alice')
+print_random('King')
+print_random('Kitty')
+print_random('One')
+print_random('Queen')
 
